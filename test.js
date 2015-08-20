f499e1fabd98ca4977a7bc856478d826fe10f05d
ryanhuynh8 <ryan.huyhuynh@gmail.com>
2015-08-20 13:46:17 +0700

    Update test.js

--- test.js
+++ test.js
@@ -50,7 +50,7 @@ test.describe('Main Page', function() {
         driver.findElement(webdriver.By.id('password')).sendKeys('123');
         driver.findElement(webdriver.By.className('btn-primary')).click();
 
-        driver.sleep(15000);
+        driver.sleep(10000);
 
         driver.findElement(webdriver.By.css('#topnav > ul > li:nth-child(3)')).getText().then(function (text) {
             text.should.be.equal('Welcome, Mike Phan');
@@ -96,31 +96,78 @@ test.describe('Main Page', function() {
 //     });
 // });
 
-test.describe('New Task Page', function() {
-    test.it('should load new task page', function(done) {
-        driver.findElement(webdriver.By.id('headerbardropdown')).click();
-        driver.findElement(webdriver.By.css('#headerbar > div > div > div.col-xs-7.col-sm-2 > a')).click();
+// test.describe('New Task Page', function() {
+//     test.it('should load new task page', function(done) {
+//         driver.findElement(webdriver.By.id('headerbardropdown')).click();
+//         driver.findElement(webdriver.By.css('#headerbar > div > div > div.col-xs-7.col-sm-2 > a')).click();
         
-        driver.sleep(3000);
+//         driver.sleep(3000);
+        
+//         driver.findElement(webdriver.By.css('#page-heading > h1')).getText().then(function (text) {
+//             text.should.be.equal('New Task');
+//         });
+//     });
+    
+//     test.it('should disable create button when loaded', function(done) {
+//         driver.findElement(webdriver.By.css('#create_task_button')).getAttribute('disabled').then(function (val) {
+//             val.should.be.equal('true');
+//         });
+//     });
+    
+//     test.it('should disable create button even if we select assignee and date', function(done) {
+//         var select_item = '#wrap > div.container-fluid.ng-scope > form > fieldset > div:nth-child(2) > div.col-sm-5 > select > option:nth-child(2)';
+//         var date_field = '#wrap > div.container-fluid.ng-scope > form > fieldset > div:nth-child(4) > div.col-sm-5 > div > input';
+//         driver.findElement(webdriver.By.css(select_item)).click();
+//         driver.findElement(webdriver.By.css(date_field)).sendKeys('10-10-2020');
+//         driver.findElement(webdriver.By.css('#create_task_button')).getAttribute('disabled').then(function (val) {
+//             val.should.be.equal('true');
+//         });
+//     });
+    
+//     test.it('should enable create button when we fill in all fields', function(done) {
+//         driver.findElement(webdriver.By.css('#task_description')).sendKeys('foo');
+//         driver.findElement(webdriver.By.css('#create_task_button')).getAttribute('disabled').then(function (val) {
+//             should.not.exist(val);
+//         });
+//     });
+    
+//     test.it('should disable create button again when we fill in invalid date', function(done) {
+//         var date_field = '#wrap > div.container-fluid.ng-scope > form > fieldset > div:nth-child(4) > div.col-sm-5 > div > input';
+//         driver.findElement(webdriver.By.css(date_field)).clear();
+//         driver.findElement(webdriver.By.css(date_field)).sendKeys('10-10-2000');
+//         driver.findElement(webdriver.By.css('#create_task_button')).getAttribute('disabled').then(function (val) {
+//             val.should.be.equal('true');
+//         });
+//     });
+    
+//     test.it('should be able to post new task', function(done) {
+//         var date_field = '#wrap > div.container-fluid.ng-scope > form > fieldset > div:nth-child(4) > div.col-sm-5 > div > input';
+//         driver.findElement(webdriver.By.css(date_field)).clear();
+//         driver.findElement(webdriver.By.css(date_field)).sendKeys('10-10-2020');
+//         driver.findElement(webdriver.By.css('#create_task_button')).click();
+        
+//         driver.wait(function () {
+//             return driver.isElementPresent(webdriver.By.className("alert"));
+//         }, 5000);
+        
+//         driver.findElement(webdriver.By.className('alert')).getText().then(function (text) {
+//             text.should.be.equal('New task added successfully. Redirecting to dashboard now...');
+//         });
+        
+//         driver.sleep(2000);
+//     });
+// });
+
+test.describe('Employee List Page', function() {
+    test.it('should load employee list page', function(done) {
+        driver.navigate().to(baseUrl);
+        driver.findElement(webdriver.By.css('#sidebar > li:nth-child(5) > a')).click();
+        driver.findElement(webdriver.By.css('#sidebar > li.ng-scope.hasChild.open > ul > li:nth-child(2) > a')).click();
+        
+        driver.sleep(2000);
         
         driver.findElement(webdriver.By.css('#page-heading > h1')).getText().then(function (text) {
-            text.should.be.equal('New Task');
-        });
-    });
-    
-    test.it('should disable create button when loaded', function(done) {
-        driver.findElement(webdriver.By.css('#create_task_button')).getAttribute('disabled').then(function (val) {
-            val.should.be.equal('true');
-        });
-    });
-    
-    test.it('should disable create button even if we select assignee and date', function(done) {
-        var select_item = '#wrap > div.container-fluid.ng-scope > form > fieldset > div:nth-child(2) > div.col-sm-5 > select > option:nth-child(2)';
-        var date_field = '#wrap > div.container-fluid.ng-scope > form > fieldset > div:nth-child(4) > div.col-sm-5 > div > input';
-        driver.findElement(webdriver.By.css(select_item)).click();
-        driver.findElement(webdriver.By.css(date_field)).sendKeys('10-10-2020');
-        driver.findElement(webdriver.By.css('#create_task_button')).getAttribute('disabled').then(function (val) {
-            val.should.be.equal('true');
+            text.should.be.equal('Employee List');
         });
     });
 });
